1c72bd84d6005914c608361b73a051abc558074e
Penjoor9 <141985297+Penjoor9@users.noreply.github.com>
2024-03-22 11:12:51 +0600

    Add files via upload

---
+++ linear.py
@@ -0,0 +1,16 @@
+# searching
+# sorting
+
+# !problrm 1
+# ?input
+user_input = [1,2,3,4,5,6,7,8,9,11]
+# ?Q: check to see if a certian number exist in the user input array
+n = 14
+
+# linear search
+for e in user_input:
+    if e == n:
+        print('found')
+    else:
+        print('not found')
+#if else, for loop, print, calculation(+,-)
