d84efc2f7575a1fa119c177d040cb30de88190fd
Penjoor9 <141985297+Penjoor9@users.noreply.github.com>
2024-04-26 11:56:04 +0600

    Add files via upload

--- b.py
+++ b.py
@@ -7,3 +7,18 @@ for i in input_str:
     if i ==")":
         floor_index= floor_index-1
 print('the final floor is',floor_index)
+# problem 2
+def is_valid_brackets(input_str):
+    stack = []
+    brackets = {'(': ')', ')': '('}
+    for char in input_str:
+        if char in brackets:
+            if stack and brackets[char] == stack[-1]:
+                stack.pop()
+            else:
+                stack.append(char)
+    return not stack
+
+# Example usage
+input_str = "()(()"
+print(is_valid_brackets(input_str))
