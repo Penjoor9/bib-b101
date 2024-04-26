333718345f4b74919f193d27d78db9e0f84b9091
Penjoor9 <141985297+Penjoor9@users.noreply.github.com>
2024-04-26 12:29:05 +0600

    Add files via upload

--- b.py
+++ b.py
@@ -37,3 +37,16 @@ if length !=0 :
     print("False")
 else:
     print("True")
+# stack
+input_str = ")))"
+stack = []
+for char in input_str:
+  if char == "(":
+      stack.append(char)
+      if char == ")":
+       stack.pop()
+length = len(stack)
+if length == 0:
+    print('True')
+else:
+    print("False")
